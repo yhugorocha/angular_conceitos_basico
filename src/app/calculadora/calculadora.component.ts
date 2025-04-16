4cc4e65064e1c598064934b8ae67e793d0ad0161
Hugo Silva <jose.silva@extreme.digital>
2025-04-16 14:12:51 -0300

    feat: Learning basic concepts of form and conditional rendering

--- src/app/calculadora/calculadora.component.ts
+++ src/app/calculadora/calculadora.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-calculadora',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculadora.component.html',
   styleUrl: './calculadora.component.scss'
 })
 export class CalculadoraComponent {
 
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    this.resultado = this.numero1 + this.numero2;
+  }
 }
--- src/app/helloword/helloworld.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'hello-world',
-  template: '<h1>Hello World 2!</h1>'
-})
-export class HelloWorldComponent{
-
-}
